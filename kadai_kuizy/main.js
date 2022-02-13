1349e42c430677b0c79256eb8045bf59943fa26a
Yamagishi Rin <rinrin0408@YamagishinoMacBook-Pro.local>
2022-02-14 02:43:13 +0900

    コードを整理

--- kadai_kuizy/main.js
+++ kadai_kuizy/main.js
@@ -1,45 +1,57 @@
 const choiceArray = [
-  ['たかなわ', 'こうわ', 'たかわ']
+  ['たかなわ', 'たかわ', 'こうわ'],
+  ['かめいど', 'かめと', 'かめど'],
+  ['こうじまち', 'おかとまち', 'かゆまち'],
+  ['おなりもん', 'ごせいもん', 'おかどもん'],
+  ['とどろき', 'たたら', 'たたりき'],
+  ['しゃくじい', 'いじい', 'せきこうい'],
+  ['ぞうしき', 'ざっしょく', 'ざっしき'],
+  ['おかちまち', 'ごしろちょう', 'みとちょう'],
+  ['ししぼね', 'ろっこつ', 'しこね'],
+  ['こぐれ', 'こしゃく', 'こばく']
+];
+const correctArray = [
+  'たかなわ', 'かめいど', 'こうじまち', 'おなりもん', 'とどろき', 'しゃくじい', 'ぞうしき', 'おかちまち', 'ししぼね', 'こぐれ'
 ];
 const explanationArray = [
   '正解は「たかなわ」です!', 
-  '正解は「」です!',
-  '正解は「」です!',
-  '正解は「」です!',
-  '正解は「」です!',
-  '正解は「」です!',
-  '正解は「」です!',
-  '正解は「」です!',
+  '正解は「かめいど」です!',
+  '正解は「こうじまち」です!',
+  '正解は「おなりもん」です!',
+  '正解は「とどろき」です!',
+  '正解は「しゃくじい」です!',
+  '正解は「ぞうしき」です!',
+  '正解は「おかちまち」です!',
   '江戸川区にあります。',
-  '正解は「」です!'];
+  '正解は「こぐれ」です!'
+];
+
 for(let i = 0; i < 1; i++){
+  document.getElementById(`title${i}`).innerHTML = `${i + 1}. この地名はなんて読む?`;
   for(let j = 0; j < 3; j++){
-    let choice = document.getElementById(`choice${i}-${j}`);
-    let choiceCorrect = document.getElementById(`choice${i}-${0}`);
+    let choice = document.getElementById(`choice${i}${j}`); //選択肢
+    let choiceCorrect = document.getElementById(`choice${i}${0}`); //正解の選択肢
     let answerBox = document.getElementById(`answer-box${i}`);
     let answer = document.getElementById(`answer${i}`);
     let explanation = document.getElementById(`explanation${i}`);
-    choice.addEventListener('click', () => {
-      if(choice.innerHTML === choiceArray[i][0]){
+    choice.innerHTML = choiceArray[i][j]; //選択肢の表示
+    choice.addEventListener('click', () => { //クリック時の動作
+      if(choice.innerHTML === correctArray[i]){ //正解
         answer.innerHTML = '正解!';
         answer.style.borderBottomColor = 'blue';
       }
-      else{
+      else{ //不正解
         choice.style.color = 'white';
         choice.style.backgroundColor = 'red';
-        document.getElementById(`answer${i}`).innerHTML = '不正解!'
+        answer.innerHTML = '不正解!'
         answer.style.borderBottomColor = 'red';
       }
       choiceCorrect.style.color = 'white';
       choiceCorrect.style.backgroundColor = 'blue';
       answerBox.style.display = 'block';
       explanation.innerHTML = explanationArray[i];
-
-      // 正解は「たかなわ」です！
+      //未実装一覧
       // ボタンを押したらページが動く
-      // 正解は背景色が青、不正解は赤 正解と選んだ不正解の文字色が白
-      // 回答BOXが出現
-      // 正解は青の下線 不正解は赤の下線
       // 一度クリックしたら選べない
     })
   }
